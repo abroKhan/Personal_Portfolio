a6901d05f94087fdeebce7fc62bba7d9aeb17da9
abrokhan149@gmail.com <abrokhan149@gmail.com>
2022-09-16 09:39:40 +0500

    commit

--- Js/script.js
+++ Js/script.js
@@ -16,5 +16,26 @@ const navLink = document.querySelectorAll(".nav__link");
 function linkAction() {
   navMenu.classList.remove("show");
 }
-
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// < === Section Active Link === >
+
+const sections = Document.querySelectorAll("sections[id]");
+window.addEventListener("scroll", scrollActive);
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add('active');
+    }
+    else{
+        document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove('active');
+        
+    }
+  });
+}
